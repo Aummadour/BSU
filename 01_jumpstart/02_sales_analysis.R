a87626767d798a38be84b7ecf23acefb80c01304
JB <jbs045@shsu.edu>
2020-09-24 19:14:19 -0500

    New Packages Grabbed 3 data tables and joined into a flat table wrangled data to prep for visualizations

---
+++ 01_jumpstart/02_sales_analysis.R
@@ -0,0 +1,136 @@
+# DS4B 101-R: R FOR BUSINESS ANALYSIS ----
+# JUMPSTART: First Sales Analysis ----
+
+# 1.0 Load libraries ----
+
+# Work horse packages
+library(tidyverse)
+library(lubridate)
+
+# theme_tq()
+library(tidyquant)
+
+# Excel Files
+library(readxl)
+library(writexl)
+
+
+
+
+# 2.0 Importing Files ----
+
+bikes_tbl <- read_excel("00_data/bike_sales/data_raw/bikes.xlsx")
+
+bikeshops_tbl <- read_excel("00_data/bike_sales/data_raw/bikeshops.xlsx")
+
+orderlines_tbl <- read_excel("00_data/bike_sales/data_raw/orderlines.xlsx")
+
+
+# 3.0 Examining Data ----
+
+
+glimpse(bikes_tbl)
+view(bikeshops_tbl)
+
+
+# 4.0 Joining Data ----
+
+left_join(orderlines_tbl, bikes_tbl, by = c("product.id" = "bike.id"))
+
+bike_orderlines_joined_tbl <- orderlines_tbl %>% 
+    left_join(bikes_tbl, by = c("product.id" = "bike.id")) %>%
+    left_join(bikeshops_tbl, by = c("customer.id" = "bikeshop.id"))
+
+# 5.0 Wrangling Data ----
+
+bike_orderlines_wrangled_tbl <- bike_orderlines_joined_tbl %>%
+    
+        #   Separate description into category.1, category.2 and frame.material
+    separate(description, 
+             into = c("category.1", "category.2", "frame.material"),
+             sep = " - ",
+             remove = TRUE) %>%
+    
+        #   Separate location into city and state
+    separate(location, 
+             into = c("city", "state"),
+             sep = ", ",
+             remove = FALSE)  %>%
+    
+        #   Mutate:  Price Extended = Qty * Price
+    mutate(total.price = price * quantity) %>%
+    
+        #   Select: to pick what we want to keep
+    select(-...1, -location) %>%
+    select(-ends_with(".id")) %>%
+
+        #   Grabbing order.id from bike_order_lines_joined_tbl to add order.id that was removed
+    bind_cols(bike_orderlines_joined_tbl %>% select(order.id)) %>%
+    
+    
+        #   Reorder columns
+        select(contains("date"), contains("id"), contains("order"),
+               quantity, price, total.price, 
+               everything()) %>%
+    
+        # Renaming columns
+    rename(order_date = order.date) %>%
+    set_names(names(.) %>% str_replace_all("\\.", "_")) 
+
+# 6.0 Business Insights ----
+
+
+# 6.1 Sales by Year ----
+
+# Step 1 - Manipulate
+
+sales_by_year_tbl <- bike_orderlines_wrangled_tbl %>% 
+    
+    # Select columns to focus on and add a year column
+    select(order_date, total_price) %>%
+    mutate(year = year(order_date)) %>%
+    
+    
+    # Grouping by year and summarizing sales
+    group_by(year) %>%
+    summarize(sales = sum(total_price)) %>%
+    ungroup() %>%
+    
+    # Format into a dollar amount
+    mutate(sales_text = scales::dollar(sales))
+
+# Step 2 - Visualize
+
+sales_by_year_tbl %>%
+        
+        #Set up canvas with year (x-axis) and sales (y-axis)
+    ggplot(aes(x = year, y = sales)) +
+    
+        # Geometries
+        geom_col(fill = "#2C3E50") +
+        geom_label(aes(label = sales_text)) +
+        geom_smooth(method = "lm", se = FALSE)
+
+# 6.2 Sales by Year and Category 2 ----
+
+
+# Step 1 - Manipulate
+
+
+
+
+# Step 2 - Visualize
+
+
+
+
+# 7.0 Writing Files ----
+
+
+# 7.1 Excel ----
+
+
+# 7.2 CSV ----
+
+
+# 7.3 RDS ----
